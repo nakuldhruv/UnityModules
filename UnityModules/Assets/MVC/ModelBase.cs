28a01826080d33dea1f1523616dbff87e2f17a41
liuzihan <liuzihan@zxs.com>
2026-06-15 09:32:51 +0800

    MVC View and Model Base

---
+++ UnityModules/Assets/MVC/ModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+namespace MVC
+{
+    public abstract class ModelBase 
+    {
+        /// <summary>
+        /// Base64 → 字节数组 → 字符串
+        /// </summary>
+        public void Load() 
+        {
+            var encrypted = PlayerPrefs.GetString(this.GetType().FullName, string.Empty);
+            var bytes     = Convert.FromBase64String(encrypted);
+            var json      = Encoding.UTF8.GetString(bytes);
+            JsonUtility.FromJsonOverwrite(json, this);
+        }
+    
+        /// <summary>
+        /// 字符串 → 字节数组 → Base64
+        /// </summary>
+        public void Save()
+        {
+            var json      = JsonUtility.ToJson(this);
+            var bytes     = Encoding.UTF8.GetBytes(json);
+            var encrypted = Convert.ToBase64String(bytes);
+            PlayerPrefs.SetString(this.GetType().FullName, encrypted);
+        }
+    }
+}
