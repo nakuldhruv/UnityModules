274c8437651f13eab3beef8c52ae1b4c330d6256
liuzihan <liuzihan@zxs.com>
2026-06-22 10:23:04 +0800

    CameraUtil - IsOutsideCameraViewport

---
+++ UnityModules/Assets/Utils/CameraUtil.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityModules
+{
+    public static class CameraUtil
+    {
+        public static bool IsOutsideCameraViewport(Camera camera, Vector3 worldPos)
+        {
+            Vector3 viewportPoint = camera.WorldToViewportPoint(worldPos);
+            if (viewportPoint.z < 0f || viewportPoint.z > camera.farClipPlane)
+                return true;
+            return viewportPoint.x < 0 || viewportPoint.x > 1 || viewportPoint.y < 0 || viewportPoint.y > 1;
+        }
+    }
+}
