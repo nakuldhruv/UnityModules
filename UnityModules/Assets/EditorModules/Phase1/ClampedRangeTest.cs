2c3990f50aca7c32b42e972aefa18313b7b8556e
liuzihan <liuzihan@zxs.com>
2026-06-22 15:27:50 +0800

    Editor：SerializedProp, PropertyDrawer

---
+++ UnityModules/Assets/EditorModules/Phase1/ClampedRangeTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UnityModules
+{
+    public class ClampedRangeTest : MonoBehaviour
+    {
+        [ClampedRange(0, 100)]
+        public float floatRangeAttr;
+        [ClampedRange(0, 100)]
+        public int intRangeAttr;
+        
+        [Space]
+        [Header("对比：系统默认 Range")]
+        [Range(0, 100)]
+        public float defaultRange;
+    }
+}
