9af976a2458aa63ad5e040d7926d24e465d513c9
liuzihan <liuzihan@zxs.com>
2026-06-24 20:12:15 +0800

    PrefabScriptFinder

---
+++ UnityModules/Assets/Editor/PrefabScriptFinder.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityModules.Editor
+{
+    public class PrefabScriptFinder : EditorWindow
+    {
+        private string           _scriptName     = string.Empty;
+        private List<GameObject> _prefabs        = new();
+        private Vector2          _scrollPosition = Vector2.zero;
+
+        [MenuItem("Tools/PrefabScriptFinder")]
+        public static void ShowWindow()
+        {
+            GetWindow<PrefabScriptFinder>("PrefabScriptFinder");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("PrefabScriptFinder", EditorStyles.boldLabel);
+            EditorGUILayout.BeginHorizontal();
+            _scriptName = EditorGUILayout.TextField("Script Name:", _scriptName);
+            if (GUILayout.Button("Search", GUILayout.Width(100))) 
+                SearchPrefabs();
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            if (_prefabs.Count > 0)
+            {
+                GUILayout.Label("Results:",  EditorStyles.boldLabel);
+                _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+                foreach (var result in _prefabs)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.ObjectField(result, typeof(GameObject), false);
+
+                    if (GUILayout.Button("Ping", GUILayout.Width(100)))
+                    {
+                        EditorGUIUtility.PingObject(result);
+                        Selection.activeObject = result;
+                    }
+                    
+                    EditorGUILayout.EndHorizontal();
+                }
+                
+                EditorGUILayout.EndScrollView();
+            }
+            else
+            {
+                GUILayout.Label("Results:",  EditorStyles.boldLabel);
+            }
+        }
+
+        private void SearchPrefabs()
+        {
+            _prefabs.Clear();
+            string[] guids = AssetDatabase.FindAssets("t:Prefab");
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                var components = prefab.GetComponentsInChildren<Component>();
+                if (!prefab) continue;
+                foreach (var component in components)
+                {
+                    if (component != null && component.GetType().Name == _scriptName)
+                    {
+                        _prefabs.Add(component.gameObject);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
